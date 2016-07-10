da577b94fe33d67a743555035d166be4fbc4889b
ocramz <zocca.marco@gmail.com>
2016-07-10 20:48:49 +0200

    compile b++++

---
+++ petsc-hs/docker/update-petsc-hs-0.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# # NB : PETSc/SLEPc environment variables must be already set at this stage
+# printenv | grep PETSC ..
+
+# STACK_ARGS="$STACK_ARGS"
+# PETSC_DIR="$PETSC_DIR"
+# SLEPC_DIR="$SLEPC_DIR"
+# PETSC_ARCH="$PETSC_ARCH"
+# SLEPC_ARCH="$SLEPC_ARCH"
+
+echo "== ENVIRONMENT :"
+printenv
+
+echo "=== ls -lsA"
+ls -lsA
+
+echo "=== pwd"
+pwd
+
+
+# retrieve latest source
+echo "=== git clone petsc-hs"
+git clone https://github.com/ocramz/petsc-hs.git
+
+echo "=== cd petsc-hs"
+cd petsc-hs
+
+
+# echo "=== stack setup"
+# stack setup
+
+# echo "=== stack install c2hs"
+# stack install c2hs
+
+
+# generate and interpret c2hs script (architecture-dependent types)
+echo "=== ./c2hs-build.sh"
+./c2hs-build.sh ${PETSC_DIR} ${PETSC_ARCH} ${SLEPC_DIR} ${SLEPC_ARCH} ${PWD}/src/Numerical/PETSc/Internal/C2HsGen
+
+echo "=== ./stack-build.sh"
+./stack-build.sh "$STACK_ARGS" "$PETSC_DIR" "$PETSC_ARCH" "$SLEPC_DIR" "$SLEPC_ARCH"
